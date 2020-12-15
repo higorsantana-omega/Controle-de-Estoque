df5d2ac68d1af19187bd1732a9a7cbbae8da0bd7
higorsantana-omega <omegadosgames02@gmail.com>
2020-12-15 14:04:54 -0300

    init db / view

--- banco_de_dados.py
+++ banco_de_dados.py
@@ -34,28 +34,50 @@ class Banco():
     def fetchall(self):
         return Banco.cursor.fetchall()
     
+    # commit
     def persist(self):
         if Banco.connected:
             Banco.cursor.commit()
             return True
         else:
             return False
-    # cursor.execute('CREATE TABLE IF NOT EXISTS produto(
-    #     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    #     nome_produto VARCHAR(50) NOT NULL,
-    #     marca VARCHAR(20),
-    #     unidade VARCHAR(10),
-    #     localizao_estoque VARCHAR(30),
-    #     estoque_minimo INTEGER,
-    #     estoque_maximo INTEGER,
-    #     peso_kg REAL,
-    #     valor_venda REAL,
-    #     valor_custO REAL,
-    #     tamanho_produto INTEGER,
-    #     origem VARCHAR(40),
-    #     classificao VARCHAR(40),
-    #     situacao VARCHAR(10),
-    #     tipo VARCHAR(10),
-    #     fornecedor VARCHAR(30),
-    #     estoque_inicial INTEGER,
-    # )')
+
+    # Criar db
+def InitDB():
+    trans = Banco()
+    trans.connect() 
+    trans.execute('CREATE TABLE IF NOT EXISTS produto(
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        nome_produto VARCHAR(50) NOT NULL,
+        marca VARCHAR(20),
+        unidade VARCHAR(10),
+        localizao_estoque VARCHAR(30),
+        estoque_minimo INTEGER,
+        estoque_maximo INTEGER,
+        peso_kg REAL,
+        valor_venda REAL,
+        valor_custO REAL,
+        tamanho_produto INTEGER,
+        origem VARCHAR(40),
+        classificao VARCHAR(40),
+        situacao VARCHAR(10),
+        tipo VARCHAR(10),
+        fornecedor VARCHAR(30),
+        estoque_inicial INTEGER,
+    )')
+    trans.persist()
+    trans.disconnect()
+
+InitDB()
+
+# Visualizar dados
+def view():
+    trans = Banco()
+    trans.connect()
+
+    trans.execute('SELECT * FROM produto')
+
+    rows = trans.fetchall()
+    trans.disconnect()
+    return rows
+
